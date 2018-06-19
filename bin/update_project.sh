68b13aab9397dc033647d7be8968a5115d65dc46
runphp <runphp@qq.com>
2018-06-19 10:29:56 +0800

    add composer up

--- bin/update_project.sh
+++ bin/update_project.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 
-
 cd /var/www/api.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/docker-php7.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/mall.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/manage.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/passport.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/so.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/uc.eelly.com
 git pull -X theirs origin master
+composer up -vvv
 
 cd /var/www/www.eelly.com
 git pull -X theirs origin master
+composer up -vvv
